2f32b383de96c4595db66da970e7e1a3a8574f3e
Namdak Tonpa <maxim@synrc.com>
2026-06-30 16:37:26 +0300

    book

---
+++ book/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rm -f *.toc
+rm -f *.aux
+rm -f *.log
+rm -f *.out
---
+++ book/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+pdflatex --interaction=nonstopmode erp.tex
+pdflatex --interaction=nonstopmode erp.tex
+exit 0
