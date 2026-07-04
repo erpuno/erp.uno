75663150e6450be5afd636316b53aabe82a9f8bc
Namdak Tonpa <maxim@synrc.com>
2026-07-04 14:29:26 +0300

    wip

--- cd/ns.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete namespace erp-uno
-kubectl create namespace erp-uno
-kubectl apply -n erp-uno -f erp-demo.yaml
-helm template erp-uno . --values values-demo.yaml > erp-demo.yaml
-kubectl port-forward -n erp-uno svc/prometheus-demo 9090:9090 # http://localhost:9090
-kubectl get all -n erp-uno
